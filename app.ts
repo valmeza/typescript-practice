e4b102bd19f1871181a31b14aad343cac688361e
Valeria Meza <valeriameza173@gmail.com>
2020-11-26 15:37:56 -0600

    core types

--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
-console.log("This is connected!");
+// object
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+} = {
+  name: "Valeria",
+  age: 25,
+  hobbies: ["sports", "cooking"],
+};
+
+console.log(person.name);
+
+let favoriteActivities: string[]; // strict
+favoriteActivities = ["some activity", "another activity"];
+
+let anythingYouWantArray: any[]; // flexible
+anythingYouWantArray = [1, true, "free", 4];
+
+for (let hobby of person.hobbies) {
+  console.log(hobby);
+}
---
+++ basics.ts
@@ -0,0 +1,6 @@
+//core types
+let number: number; // all numbers, no differentiation between integers or floats
+let word: string; // all text values
+let ifTrue: boolean; // truthy or falsy values
+let myObject: object; // any js object
+let myArray: [] // any js array, type can be flexible or strict
